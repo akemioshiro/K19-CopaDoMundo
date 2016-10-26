1732dad3bb78c2c7f2a7e05f27c83852ba044dfb
akemioshiro@hotmail.com <akemioshiro@hotmail.com>
2016-10-26 16:52:40 -0200

    Entidades e Repositorios

---
+++ K19-CopaDoMundo/Models/Selecao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace K19_CopaDoMundo.Models
+{
+    [Table("Selecoes")]
+    public class Selecao
+    {
+        public int Id { get; set; }
+        public String Pais { get; set; }
+        public string Tecnico { get; set; }
+
+        public virtual List<Jogador> Jogadores { get; set; }
+    }
+}
